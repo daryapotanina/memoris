b7b7e12e090855ce6faa2535ddd9750295b2cbab
Darya Potanina <potanina.dasha@ya.ru>
2021-09-19 14:42:57 +0300

    clik on card

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+function shuffle(array: any[]): any[] {
+  return array
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+}
+
+let cards = shuffle([...Array(30).keys()].concat([...Array(30).keys()]));
+
 function App() {
-  let cards = [...Array(30).keys()].concat([...Array(30).keys()]);
+  const [opened, setOpened] = useState<number[]>([]);
+
+  function handleCardClick(item: number, i: number) {
+    setOpened([i]);
+  }
+
   return (
     <div className="App">
-      <header className="Memosis"></header>
+      <header className="Memosis">
+        <pre>opened: {JSON.stringify(opened)}</pre>
+      </header>
       <div className="gameWrapper">
         <div className="game">
           {cards.map((item, i) => {
+            let isOpen = opened.includes(i);
             return (
-              <div key={`card_${i}`} className="card">
-                {item}
+              <div
+                onClick={() => handleCardClick(item, i)}
+                key={`card_${i}`}
+                className="card"
+              >
+                <img
+                  className={"card_img"}
+                  style={{ opacity: isOpen ? 1 : 0 }}
+                  src="/pics/mone_kuvshinki.jpg"
+                  alt=""
+                />
+                <div className="debug_index">{item}</div>
               </div>
             );
           })}
